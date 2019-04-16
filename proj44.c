4f811ade08eb20e8600c2f2d1796610ba08110a8
Ashishxharma <49365639+Ashishxharma@users.noreply.github.com>
2019-04-16 22:08:47 +0530

    Update proj44.c

--- proj44.c
+++ proj44.c
@@ -1,25 +1,15 @@
-#include<pthread.h>
 #include<stdio.h>
-#include<unistd.h>
-#include<sys/wait.h>
 #include<stdlib.h>
-
 #define NEW 0
 #define READY 1
 #define WAITING 2
 #define RUNNING 3
 #define EXIT 4
-
 int presenttime=0,time_in_exec=0,EXITED=0,TQ,Context_Switch_Permitted=1,n;
 int  itr=1,ps=0,noe=0;
 struct process 
 {
-	int pid;
-	int state;
-	int timeleft;
-        int et;
-	int at;
-	int wt,tat,ct,execution_time;
+	int pid,state,timeleft,et,at,wt,tat,ct,execution_time;
 	struct process *prev;
 	struct process *next;
 } *process_array;
@@ -42,7 +32,6 @@ void SortTimeleft()
             }
         }
     }
-
 }
 void enqueue(struct process *p)
 {
@@ -82,14 +71,12 @@ if(itr==1)
         noe=0;
     }
            for(int i=0;i<n;i++)
-	   {
-                   
+	   {  
 		   if(process_array[i].state == NEW && presenttime>=process_array[i].at)
 		   {      
 			   
 			   enqueue(&process_array[i]);
-			   process_array[i].state=READY;
-			   
+			   process_array[i].state=READY;   
                     }
                   if(process_array[i].state==EXIT)
                   {
@@ -102,16 +89,11 @@ if(itr==1)
 	   }
 	   if(count==n)
 	   {
-		  EXITED=1; 
-		  
+		  EXITED=1;   
 	   }
-
 }
-
 int main()
 {
-
-
 Ready =(struct Queue*) malloc(sizeof(struct Queue));
 	printf("Please enter No of processes to schedule");
 	scanf("%d",&n);
@@ -126,15 +108,13 @@ Ready =(struct Queue*) malloc(sizeof(struct Queue));
 	     scanf("%d",&(process_array[i].timeleft)); 
 	     
             process_array[i].execution_time=process_array[i].timeleft;
-	    process_array[i].state=NEW;
-	        
+	    process_array[i].state=NEW;       
 	}
  struct process key; 
     int i,j;
     for (i = 1; i < n; i++) { 
         key = process_array[i]; 
         j = i - 1; 
-  
         while (j >= 0 && process_array[j].at > key.at) { 
             process_array[j + 1] = process_array[j]; 
             j = j - 1; 
@@ -148,7 +128,6 @@ while(1)
         checkqueue();
         if(EXITED==1 || itr>2)
         {
-
                     break;      
         }
         if(itr==1)
@@ -161,7 +140,6 @@ while(1)
        }
         if(itr==1&&ps==n&&Context_Switch_Permitted==1)
        {  
-
                itr++;
                ps=0;
                for(int i=0;i<n;i++)
@@ -244,18 +222,13 @@ while(1)
             else
           { 
               Context_Switch_Permitted=0;
-	  }
-    
-          
-             
-          
+	  }   
 	}
 else
 {
 printf("%d | IDLE |",presenttime);
 presenttime++;
 }
-
 }
 SortTimeleft();
 for(int i=0;i<n;i++)
@@ -277,9 +250,7 @@ for(int i=0;i<n;i++)
 	{ 
            printf("\n\nprocess pid=%d\nct=%d\ntat=%d\nwt=%d",process_array[i].pid,process_array[i].ct,process_array[i].tat,process_array[i].wt);
            sumwt+=process_array[i].wt;
-           sumtat+=process_array[i].tat;
-	   
-                    
+           sumtat+=process_array[i].tat;            
 	}
 printf("\n\n Avergae TAT=%f \n Average WT=%f",(sumtat/(n*1.0)),(sumwt/(n*1.0)));
 }
